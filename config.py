77b89dd73cc7d1fbf30f138ebf485fcc0b1aa709
fuhu <fu.hu@live.cn>
2018-08-19 23:57:31 +0800

    update

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 16379
 REDIS_DB = 0
 
-AIP_AK = 'ATc8HsEb7Mh9FiDFrnqjoWBE'
-AIP_SK = 'zKWpPOx96xnoj7yC5svaGSLXO8LnPwn4'
+AIP_AK = 'DxIZQ7wjnukFg8FuT0ZLDi9L'
+AIP_SK = 'ZY2h6hLfqUxmE2XkAIZ8B359odyGKD97'
 
 SVC_TOKEN_URL = 'https://openapi.baidu.com/oauth/2.0/token'
 SVC_ASR_URL = 'https://vop.baidu.com/server_api'
